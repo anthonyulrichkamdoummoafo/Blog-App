3edd82c0579b73d2200ef21b9a47b85be83b9d3d
anthonyulrichkamdoummoafo <91798449+anthonyulrichkamdoummoafo@users.noreply.github.com>
2023-12-13 16:56:25 +0100

    Add files via upload

--- django_project/django_project/blog/views.py
+++ django_project/django_project/blog/views.py
@@ -4,13 +4,13 @@ from django.http import HttpResponse
 posts = [
     {
         'author': 'CoreyMs',
-        'title': 'Blog Post',
+        'title': 'Blog Post 1',
         'content':'Frist post content',
         'date_posted': 'August 27, 2018',
     },
     {
         'author': 'Jane Doe',
-        'title': 'Blog Post',
+        'title': 'Blog Post 2',
         'content':'Second post content',
         'date_posted': 'August 28, 2018',
     }
@@ -23,6 +23,6 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
 
 # Create your views here.
